fc1a381031447b5f029b88403e55724270195d76
cadenwurzbacher <caden@cadenwurzbacher.com>
2024-12-09 15:28:32 -0600

    fixed reinjected rerun bug

--- app.py
+++ app.py
@@ -265,4 +265,4 @@ if st.button("Clear all events"):
     session.query(EventRecord).delete()
     session.commit()
     st.success("All events have been cleared.")
-    st.experimental_rerun()
+    st.rerun()
